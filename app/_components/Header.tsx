873b470c10471a27cc9b41e6fa764ee407eb0f63
Empty <emptypageworking@gmail.com>
2026-06-24 01:51:57 +0700

    Feat: append header

---
+++ app/_components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='bg-[#E5F0FC] p-10 flex border-b border-[#DEDBE2] hover:bg-[#D4EBFD] cursor-pointer'>
+    </header>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Header from "@/app/_components/Header";
+
+export default function Home() {
+  return (
+    <main className="bg-white min-h-screen">
+      <div className="relative z-10 flex h-full flex-col">
+        <Header />
+        <div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
